38f8961afed925f1dccd6e25c2b65b2c5ba3c786
aldhyyy <aldyzkobf@gmail.com>
2020-09-01 02:22:53 +0800

    added animation

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,52 +1,78 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./TodoItems.css";
 
-function TodoItems({ todosFilter, deleteTodo, checkTodo }) {
-  let todoList = null;
-
-  if (todosFilter.length <= 0) {
-    todoList = <p className="todo-empty">~ Empty Todo ~</p>;
-  } else {
-    todoList = todosFilter.map((todo) => {
-      return (
-        <div className="todo-item" key={todo.id}>
-          <div className="todo-item__left">
+const List = ({ todo, checkTodo, deleteTodo, ...props }) => {
+  const nodeRef = React.useRef(null);
+  return (
+    <CSSTransition
+      timeout={300}
+      classNames="todo"
+      key={todo.id}
+      {...props}
+      nodeRef={nodeRef}
+    >
+      <div className="todo-item" ref={nodeRef}>
+        <div className="todo-item__left">
+          <label
+            className={
+              todo.completed
+                ? "todo-item__label todo-checked"
+                : "todo-item__label"
+            }
+            style={{ display: todo.edited && "none" }}
+          >
             <input
               type="checkbox"
               onChange={(e) => checkTodo(todo.id, e)}
               checked={todo.completed}
             />
-            <div
-              className={
-                todo.completed
-                  ? "todo-item__label todo-checked"
-                  : "todo-item__label"
-              }
-              style={{ display: todo.edited && "none" }}
-            >
-              {todo.content}
-            </div>
-            <input
-              type="text"
-              className={
-                todo.edited ? "todo-item__edit show" : "todo-item__edit"
-              }
-              defaultValue={todo.content}
-            />
-          </div>
-          <div className="todo-item__right">
-            <div
-              className="todo-remove__button"
-              onClick={(e) => deleteTodo(todo.id, e)}
-            >
-              <span role="img" aria-label="emoticon">
-                ❌
-              </span>
-            </div>
+            {todo.content}
+          </label>
+          <input
+            type="text"
+            className={todo.edited ? "todo-item__edit show" : "todo-item__edit"}
+            defaultValue={todo.content}
+          />
+        </div>
+        <div className="todo-item__right">
+          <div
+            className="todo-remove__button"
+            onClick={(e) => deleteTodo(todo.id, e)}
+          >
+            <span role="img" aria-label="emoticon">
+              &times;
+            </span>
           </div>
         </div>
+      </div>
+    </CSSTransition>
+  );
+};
+
+function TodoItems({ todosFilter, deleteTodo, checkTodo }) {
+  let todoList = null;
+  const nodeRef = React.useRef(null);
+
+  if (todosFilter.length > 0) {
+    todoList = todosFilter.map((todo) => {
+      return (
+        <List
+          todo={todo}
+          key={todo.id}
+          checkTodo={checkTodo}
+          deleteTodo={deleteTodo}
+        />
       );
     });
+  } else {
+    todoList = (
+      <CSSTransition classNames="todo" timeout={300} nodeRef={nodeRef}>
+        <p className="todo-empty" ref={nodeRef}>
+          ~ Empty Todo ~
+        </p>
+      </CSSTransition>
+    );
   }
 
   return (
@@ -54,7 +80,9 @@ function TodoItems({ todosFilter, deleteTodo, checkTodo }) {
       <div className="todo-header">
         <p className="title">My Todo List</p>
       </div>
-      <div className="todo-parent">{todoList}</div>
+      <div className="todo-parent">
+        <TransitionGroup component={null}>{todoList}</TransitionGroup>
+      </div>
     </div>
   );
 }
