c72791abcf4d791c5e8ca97987116db356cf1158
aldhyyy <aldyzkobf@gmail.com>
2020-09-01 14:31:41 +0800

    using scss module

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../logo.svg";
-import "./Header.scss";
+import Logo from "../logo.svg";
+import { header, logo, title } from "./Header.module.scss";
 
 function Header() {
   return (
-    <header className="header">
-      <img src={logo} className="logo" alt="logo" />
-      <h1 className="title">TODO LIST</h1>
+    <header className={header}>
+      <img src={Logo} className={logo} alt="logo" />
+      <h1 className={title}>TODO LIST</h1>
     </header>
   );
 }
